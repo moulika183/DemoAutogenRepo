30f2851edc139a0e08c69796f1a1ecf9ecf77019
Moulika <99651718+moulika183@users.noreply.github.com>
2025-09-25 14:57:49 +0530

    Update ProductService/Dtos/ProductDto.cs generated by AutoGen

--- ProductService/Dtos/ProductDto.cs
+++ ProductService/Dtos/ProductDto.cs
@@ -4,9 +4,9 @@ namespace ProductService.Dtos;
 public class ProductDto
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = default!;
+    public string Name { get; set; } = null!;
     public string? Description { get; set; }
     public decimal Price { get; set; }
-    public string SKU { get; set; } = default!;
+    public string? Category { get; set; }
 }
 ```
