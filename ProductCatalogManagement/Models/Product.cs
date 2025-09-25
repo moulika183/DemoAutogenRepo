6a985d8600afb64b3b19e232f740b8a4d47c90eb
Moulika <99651718+moulika183@users.noreply.github.com>
2025-09-25 15:29:31 +0530

    Add ProductCatalogManagement/Models/Product.cs generated by AutoGen

---
+++ ProductCatalogManagement/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace ProductCatalogManagement.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; }
+    }
+}
