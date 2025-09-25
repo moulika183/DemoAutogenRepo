606bb41a5e0989e3f90775a17bcd6816bb86961a
Moulika <99651718+moulika183@users.noreply.github.com>
2025-09-25 14:58:18 +0530

    Add OrderService/Dtos/CreateOrderDto.cs generated by AutoGen

---
+++ OrderService/Dtos/CreateOrderDto.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderService.Dtos
+{
+    public class CreateOrderDto
+    {
+        [Required]
+        public string CustomerName { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(1)]
+        public List<CreateOrderItemDto> Items { get; set; } = new();
+    }
+
+    public class CreateOrderItemDto
+    {
+        [Required]
+        public string ProductId { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
