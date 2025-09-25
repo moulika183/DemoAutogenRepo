4170b830e986e416d5bd6507351f9b1b8abe9931
Moulika <99651718+moulika183@users.noreply.github.com>
2025-09-25 14:46:01 +0530

    Add ProductService/Repositories/IProductRepository.cs generated by AutoGen

---
+++ ProductService/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+```csharp
+using ProductService.Models;
+
+namespace ProductService.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(Guid id);
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task DeleteAsync(Guid id);
+}
+```
