f5b0531a641d12f6003249e23760e2aa383fd669
Moulika <99651718+moulika183@users.noreply.github.com>
2025-09-25 15:12:23 +0530

    Update ProductService/Models/Product.cs generated by AutoGen

--- ProductService/Models/Product.cs
+++ ProductService/Models/Product.cs
@@ -4,9 +4,8 @@ namespace ProductService.Models;
 public class Product
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = null!;
+    public string Name { get; set; } = default!;
     public string? Description { get; set; }
     public decimal Price { get; set; }
-    public string? Category { get; set; }
 }
 ```
