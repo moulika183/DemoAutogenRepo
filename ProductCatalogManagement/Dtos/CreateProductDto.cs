7b31928f4d8b6dcb3f3abe775ab5161a101cdc27
Moulika <99651718+moulika183@users.noreply.github.com>
2025-09-25 15:29:27 +0530

    Add ProductCatalogManagement/Dtos/CreateProductDto.cs generated by AutoGen

---
+++ ProductCatalogManagement/Dtos/CreateProductDto.cs
@@ -0,0 +1,10 @@
+namespace ProductCatalogManagement.Dtos
+{
+    public class CreateProductDto
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; }
+    }
+}
