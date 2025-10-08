fa84c144e0535b938ac8be83efce1c75cd94905b
Moulika <99651718+moulika183@users.noreply.github.com>
2025-10-08 14:21:50 +0530

    Update OrderService/Dtos/PlaceOrderRequestDto.cs generated by AutoGen

--- OrderService/Dtos/PlaceOrderRequestDto.cs
+++ OrderService/Dtos/PlaceOrderRequestDto.cs
@@ -1,27 +1,26 @@
-
 using System.ComponentModel.DataAnnotations;
 
-namespace OrderService.Dtos;
-
-public class PlaceOrderRequestDto
+namespace OrderService.Dtos
 {
-    [Required]
-    public string CustomerName { get; set; } = null!;
+    public class PlaceOrderRequestDto
+    {
+        [Required]
+        public string CustomerName { get; set; } = default!;
 
-    [Required]
-    [MinLength(1)]
-    public List<OrderItemDto> Items { get; set; } = null!;
+        [Required]
+        [MinLength(1)]
+        public List<PlaceOrderItemDto> Items { get; set; } = new();
+    }
+
+    public class PlaceOrderItemDto
+    {
+        [Required]
+        public Guid ProductId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal UnitPrice { get; set; }
+    }
 }
-
-public class OrderItemDto
-{
-    [Required]
-    public string ProductName { get; set; } = null!;
-
-    [Range(1, int.MaxValue)]
-    public int Quantity { get; set; }
-
-    [Range(0, double.MaxValue)]
-    public decimal UnitPrice { get; set; }
-}
-
