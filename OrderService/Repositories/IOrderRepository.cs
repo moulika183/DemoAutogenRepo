c4fa99fa19f52bc397428ab44ecc5d39d1750dd3
Moulika <99651718+moulika183@users.noreply.github.com>
2025-10-08 14:21:30 +0530

    Update OrderService/Repositories/IOrderRepository.cs generated by AutoGen

--- OrderService/Repositories/IOrderRepository.cs
+++ OrderService/Repositories/IOrderRepository.cs
@@ -1,10 +1,10 @@
-
 using OrderService.Models;
 
-namespace OrderService.Repositories;
-
-public interface IOrderRepository
+namespace OrderService.Repositories
 {
-    Task<Order> AddAsync(Order order, CancellationToken cancellationToken);
-    // Future: Task<List<Order>> GetAllAsync(CancellationToken cancellationToken);
+    public interface IOrderRepository
+    {
+        Task AddAsync(Order order);
+        Task<Order?> GetByIdAsync(Guid id);
+    }
 }
