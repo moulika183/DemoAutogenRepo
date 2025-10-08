deeb79eb3987afecf84a1f7ce3fa279805048cda
Moulika <99651718+moulika183@users.noreply.github.com>
2025-10-08 14:21:58 +0530

    Update ProductService/Program.cs generated by AutoGen

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,33 +1,35 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using ProductService.Repositories;
 using ProductService.Services;
 using ProductService.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Services
 builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService.Services.ProductService>();
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// CORS
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder =>
+    options.AddPolicy("AllowAll", policy =>
     {
-        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
     });
 });
 
-// Add logging (already built-in)
-builder.Services.AddLogging();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// Middleware
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
@@ -36,10 +38,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors();
+app.UseCors("AllowAll");
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
